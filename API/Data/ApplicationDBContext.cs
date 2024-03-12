f9bb158474a39dba2a6757b13b114c5f4038adc2
hassan <hchoudhury20@hotmail.com>
2024-03-12 22:51:33 +0000

    create db

---
+++ API/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data{
+
+public class ApplicationDBContext:IdentityDbContext
+{
+    public ApplicationDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+    {
+        
+    }
+
+public DbSet<Car> Car {get ; set ;}
+}
+}
--- API/Models/Car.cs
+++ API/Models/Car.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Models{
-public class VehicleParameters
+public class Car
 {
-    public string Make { get; set; } // Vehicle manufacturer (e.g. audi or toyota)
-    public string Model { get; set; } // Vehicle model (e.g. a4 or corolla)
-    public string FuelType { get; set; } // Type of fuel used. Possible values: gas, diesel, electricity
-    public string Drive { get; set; } // Drive transmission. Possible values: fwd (front-wheel drive), rwd (rear-wheel drive), awd (all-wheel drive), 4wd (four-wheel drive)
-    public int? Cylinders { get; set; } // Number of cylinders in engine. Possible values: 2, 3, 4, 5, 6, 8, 10, 12, 16
-    public string Transmission { get; set; } // Type of transmission. Possible values: manual, automatic
-    public int? Year { get; set; } // Vehicle model year (e.g. 2018)
+    [Key]
+    public int Id { get; set; }
+    public string Make { get; set; }  = string.Empty;
+    public string Model { get; set; } = string.Empty;// Vehicle model (e.g. a4 or corolla)
+        public string FuelType { get; set; } = string.Empty;// Type of fuel used. Possible values: gas, diesel, electricity
+        public string Drive { get; set; } = string.Empty;// Drive transmission. Possible values: fwd (front-wheel drive), rwd (rear-wheel drive), awd (all-wheel drive), 4wd (four-wheel drive)
+        public int? Cylinders { get; set; } // Number of cylinders in engine. Possible values: 2, 3, 4, 5, 6, 8, 10, 12, 16
+    public string Transmission { get; set; } = string.Empty;// Type of transmission. Possible values: manual, automatic
+        public int? Year { get; set; } // Vehicle model year (e.g. 2018)
     public double? MinCityMpg { get; set; } // Minimum city fuel consumption (in miles per gallon)
     public double? MaxCityMpg { get; set; } // Maximum city fuel consumption (in miles per gallon)
     public double? MinHwyMpg { get; set; } // Minimum highway fuel consumption (in miles per gallon)
