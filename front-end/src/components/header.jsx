7f6415156144f90693910a21abd70b2b3464224d
hassan <hchoudhury20@hotmail.com>
2024-04-15 22:55:47 +0100

    create drop down for services

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,31 +1,37 @@
- import { jwtDecode } from "jwt-decode"
- const header= () =>{
+import { jwtDecode } from "jwt-decode"
+
+const Header = () => {
     const jwtToken = localStorage.getItem("token")
-   let  username = ""
-    if(jwtToken)
-    {
-        try
-        {
+    let username = ""
+
+    if (jwtToken) {
+        try {
             const decodedToken = jwtDecode(jwtToken);
-            username = decodedToken.given_name; 
+            username = decodedToken.given_name;
         } catch (error) {
             console.error('Error decoding JWT token:', error);
         }
     }
 
-return(
-<header>
-    <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="/login">Login</a></li>
-                {username && <li>Hello {username}</li>}
-        </ul>
-    </nav>
-</header>
-)
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li className="dropdown">
+                        <a href="#">Services</a>
+                        <div className="dropdown-content">
+                            <a href="#">Cars</a>
+                            <a href="#">Your Garage</a>
+                        </div>
+                    </li>
+                    <li><a href="/login">Login</a></li>
+                    {username && <li>Hello {username}</li>}
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
-export default header;
+export default Header;
