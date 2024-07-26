ca6bb5402984bce1fae14e69508ae78b9c6cf23c
hassan <hchoudhury20@hotmail.com>
2024-07-26 18:27:30 +0100

    add signal r react client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb35dffdf368a8eaa803dae03b605bee389b4e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9116f5030e1eeb61f3c40f0d0adddb76b7da43b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
