0ddc431b5976ab68413d9549e962c7cf330eab3b
hassan <hchoudhury20@hotmail.com>
2024-07-30 23:58:37 +0100

    create component for chatroom messages

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ad80ef3cfc40a442e6d17734351416de9d32b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deacc316ec972150c8a30acd8abdc2bf2144dfc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
